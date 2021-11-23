dca8611303f6057d888a1ee83075066980673853
Alexey Tolkov <atolkov.ext@igus.net>
2021-11-23 18:03:47 +0500

    Add project files.

---
+++ Margherita.Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Margherita.Test
+{
+    public class Tests
+    {
+        [Test]
+        void MamaMargheritaPizza()
+        {
+            var expected = "Mama Margherita, 14\" Pizza with Special Mama source, Sucuk, Pitze, Käse, Special Cheese is only $10";
+            var actual = Pizza.CreateByName("Mama Margherita").ToString();
+            Assert.Equals(expected, actual);
+        }
+
+        [Test]
+        void MamaSpecialPizza()
+        {
+            var expected = "Mama Special, 18\" Pizza with Special Mama Double Source, Zwiebel, Knoblauch, Sucuk, Pitze, Petersilie, Special Cheese x2 is only $15";
+            var actual = Pizza.CreateByName("Mama Special").ToString();
+            Assert.Equals(expected, actual);
+        }
+    }
+}
