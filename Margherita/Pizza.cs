9accb93a6f6029dab3ae92b30ca09baefe0994f2
Alexey Tolkov <atolkov.ext@igus.net>
2021-11-25 18:10:23 +0500

    mob next [ci-skip] [ci skip] [skip ci]

--- Margherita/Pizza.cs
+++ Margherita/Pizza.cs
@@ -10,15 +10,23 @@ namespace Margherita.Test
         public int Size { get; set; }
         public decimal Price { get; set; }
 
-        public static object CreateByName(string v)
+        public override string ToString()
+        {
+            return "Mama Margherita, 14\" Pizza with Special Mama source, Sucuk, Pitze, Käse, Special Cheese is only $10";
+        }
+
+        public static Pizza CreateByName(string v)
         {
             if (v == "Mama Margherita")
             {
-                return new Pizza(); // "Mama Margherita, 14\" Pizza with Special Mama source, Sucuk, Pitze, Käse, Special Cheese is only $10";
+                return new Pizza()
+                { 
+                    Name = v
+                }; // "Mama Margherita, 14\" Pizza with Special Mama source, Sucuk, Pitze, Käse, Special Cheese is only $10";
             }
             else
             { 
-                return "Mama Special, 18\" Pizza with Special Mama Double Source, Zwiebel, Knoblauch, Sucuk, Pitze, Petersilie, Special Cheese x2 is only $15";
+                return new Pizza(); //"Mama Special, 18\" Pizza with Special Mama Double Source, Zwiebel, Knoblauch, Sucuk, Pitze, Petersilie, Special Cheese x2 is only $15";
             }
         }
     }
--- Margherita/UnitTest1.cs
+++ Margherita/UnitTest1.cs
@@ -13,18 +13,18 @@ namespace Margherita.Test
         }
 
         [Test]
-        public void CheckMamaMargheritaPizzaName_Should_Be_MamaMargherita()
-        {
-            string expected = "Mama Margherita";
-            string actual = Pizza.CreateByName("Mama Margherita").Name;
-            Assert.AreEqual(expected, actual);
-        }
-
-        [Test]
         public void MamaSpecialPizza()
         {
             var expected = "Mama Special, 18\" Pizza with Special Mama Double Source, Zwiebel, Knoblauch, Sucuk, Pitze, Petersilie, Special Cheese x2 is only $15";
             var actual = Pizza.CreateByName("Mama Special").ToString();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void CheckMamaMargheritaPizzaName_Should_Be_MamaMargherita()
+        {
+            string expected = "Mama Margherita";
+            string actual = Pizza.CreateByName("Mama Margherita").Name;
             Assert.AreEqual(expected, actual);
         }
     }
